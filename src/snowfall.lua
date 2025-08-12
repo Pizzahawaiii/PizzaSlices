49bfba7fad354d77afab717638b5d733ca1567a6
Pizzahawaiii <ksorrr@gmail.com>
2025-08-12 16:26:01 +0200

    fix: disable snowfall module if bongos is installed

--- src/snowfall.lua
+++ src/snowfall.lua
@@ -6,59 +6,38 @@ PizzaSlices:RegisterModule('snowfall', function ()
     return
   end
 
+  if bongos ~= nil then
+    return
+  end
+
   function _G.ActionButtonDown(id)
     PS.OnClose = function ()
       ActionButtonUp(id)
     end
 
     local button,pagedID
-    if bongos == nil then
-      if ( BonusActionBarFrame:IsShown() ) then
-        local button = getglobal("BonusActionButton"..id);
-        if ( button:GetButtonState() == "NORMAL" ) then
-          button:SetButtonState("PUSHED");
-          UseAction(ActionButton_GetPagedID(button), 0, 0);
-        end
-        return;
-      end
-
-      button = getglobal("ActionButton"..id)
-      
-      if (button:GetButtonState() == "NORMAL" ) then
+    if ( BonusActionBarFrame:IsShown() ) then
+      local button = getglobal("BonusActionButton"..id);
+      if ( button:GetButtonState() == "NORMAL" ) then
         button:SetButtonState("PUSHED");
         UseAction(ActionButton_GetPagedID(button), 0, 0);
       end
-    else
-      button = getglobal("BActionButton"..id)
-      pagedID = BActionButton.GetPagedID(id);
-      if (button and button:GetButtonState() == "NORMAL" ) then
-        button:SetButtonState("PUSHED");
-      end
-      UseAction(pagedID, 0);
+      return;
+    end
+
+    button = getglobal("ActionButton"..id)
+    
+    if (button:GetButtonState() == "NORMAL" ) then
+      button:SetButtonState("PUSHED");
+      UseAction(ActionButton_GetPagedID(button), 0, 0);
     end
   end
 
   function _G.ActionButtonUp(id, onSelf)
     local button
-    if bongos == nil then
-      if ( BonusActionBarFrame:IsShown() ) then
-        local button = getglobal("BonusActionButton"..id);
-        if ( button:GetButtonState() == "PUSHED" ) then
-          button:SetButtonState("NORMAL");
-          if ( MacroFrame_SaveMacro ) then
-            MacroFrame_SaveMacro();
-          end
-          if ( IsCurrentAction(ActionButton_GetPagedID(button)) ) then
-            button:SetChecked(1);
-          else
-            button:SetChecked(0);
-          end
-        end
-        return;
-      end
-
-      button = getglobal("ActionButton"..id)
-      if ( button and button:GetButtonState() == "PUSHED" ) then
+    if ( BonusActionBarFrame:IsShown() ) then
+      local button = getglobal("BonusActionButton"..id);
+      if ( button:GetButtonState() == "PUSHED" ) then
         button:SetButtonState("NORMAL");
         if ( MacroFrame_SaveMacro ) then
           MacroFrame_SaveMacro();
@@ -69,14 +48,19 @@ PizzaSlices:RegisterModule('snowfall', function ()
           button:SetChecked(0);
         end
       end
-    else
-      button = getglobal("BActionButton"..id)
-      if ( button and button:GetButtonState() == "PUSHED" ) then
-        button:SetButtonState("NORMAL");
-        if ( MacroFrame_SaveMacro ) then
-          MacroFrame_SaveMacro();
-        end
-        button:SetChecked(IsCurrentAction(BActionButton.GetPagedID(id)));
+      return;
+    end
+
+    button = getglobal("ActionButton"..id)
+    if ( button and button:GetButtonState() == "PUSHED" ) then
+      button:SetButtonState("NORMAL");
+      if ( MacroFrame_SaveMacro ) then
+        MacroFrame_SaveMacro();
+      end
+      if ( IsCurrentAction(ActionButton_GetPagedID(button)) ) then
+        button:SetChecked(1);
+      else
+        button:SetChecked(0);
       end
     end
   end
